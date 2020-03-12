8b79f37973f1e7c62bb312b8db7753dfb1b47398
Nathan Burk <nate1113@comcast.net>
2020-03-12 14:48:18 -0400

    add new way to fail on error

--- grails-app/controllers/dashboard/rest/AssetController.groovy
+++ grails-app/controllers/dashboard/rest/AssetController.groovy
@@ -20,7 +20,6 @@ class AssetController {
 
         def assets = request.JSON.assets
 
-
         assets.each { asset ->
             def newAsset = new Asset()
             newAsset.assetId = asset.assetId
@@ -28,11 +27,10 @@ class AssetController {
             newAsset.tankNum = asset.tankNum
             newAsset.account = Accounts.findByNumber(asset.accountNumber.toInteger())
 
+            newAsset.save()
             if(newAsset.hasErrors()) {
                 respond([status:400, error:newAsset.errors])
             }
-
-            newAsset.save()
         }
 
         respond([status:200, message:"added new accounts"])
