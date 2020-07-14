e788ad186b2fc8121f38b9f268c9a99f320f950e
Nathan Burk <nate1113@comcast.net>
2020-07-14 18:12:46 -0400

    remove duplicate accounts and tanks controllers

--- grails-app/controllers/dashboard/rest/AccountsController.groovy
+++ grails-app/controllers/dashboard/rest/AccountsController.groovy
@@ -7,15 +7,113 @@ import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
 import grails.transaction.Transactional
 
+import static org.springframework.http.HttpStatus.CREATED
+import static org.springframework.http.HttpStatus.NOT_FOUND
+import static org.springframework.http.HttpStatus.NO_CONTENT
+import static org.springframework.http.HttpStatus.OK
+
 @Secured(["permitAll"])
 @SSLRequired
 @Transactional
 class AccountsController {
 
-    static allowedMethods = [getAccounts:"POST",addAccounts: "POST",editAccount:"POST",findByBillingType:"POST"]
+    static allowedMethods = [getAccounts:"POST",addAccounts: "POST",editAccount:"POST",findByBillingType:"POST",save: "POST", update: "PUT", delete: "DELETE"]
 
-    def index() {
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Accounts.list(params), model:[accountsCount: Accounts.count()]
+    }
 
+    def show(Accounts accounts) {
+        respond accounts
+    }
+
+    def create() {
+        respond new Accounts(params)
+    }
+
+    @Transactional
+    def save(Accounts accounts) {
+        if (accounts == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (accounts.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond accounts.errors, view:'create'
+            return
+        }
+
+        accounts.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'accounts.label', default: 'Accounts'), accounts.id])
+                redirect accounts
+            }
+            '*' { respond accounts, [status: CREATED] }
+        }
+    }
+
+    def edit(Accounts accounts) {
+        respond accounts
+    }
+
+    @Transactional
+    def update(Accounts accounts) {
+        if (accounts == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (accounts.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond accounts.errors, view:'edit'
+            return
+        }
+
+        accounts.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'accounts.label', default: 'Accounts'), accounts.id])
+                redirect accounts
+            }
+            '*'{ respond accounts, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Accounts accounts) {
+
+        if (accounts == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        accounts.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'accounts.label', default: 'Accounts'), accounts.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'accounts.label', default: 'Accounts'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
     }
 
     def getAccounts() {
